819ac19b2e99b5546b03c6765b8aafd0d2dad537
Oksana999 <oksanam999@gmail.com>
2019-06-18 09:01:19 -0500

    Add methods create and searching books

--- src/main/java/com/oksana/library/controllers/BookController.java
+++ src/main/java/com/oksana/library/controllers/BookController.java
@@ -1,13 +1,18 @@
 package com.oksana.library.controllers;
 
 import com.oksana.library.dtos.BookDto;
+import com.oksana.library.entities.Book;
 import com.oksana.library.mappers.BookMapper;
 import com.oksana.library.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -46,6 +51,65 @@ public class BookController {
         // 3. entity --> dto
         // 4. return dto на фронтент
 
+
       return  this.bookMapper.mapToDto(this.bookService.create(this.bookMapper.mapToEntity(bookDto)));
     }
+
+    @GetMapping(value = "/{id}")
+    public BookDto get(@PathVariable Long id){
+        //1. обращаемся к сервису, чтобы найти книгу
+        //2. найденную книгу превращ. в dto
+        // 3. возвращаем dto на фронтент
+//        Optional<Book> bookOpt = this.bookService.get(id);
+//        if(!bookOpt.isPresent()){
+//            throw new RuntimeException("Book was not found");
+//        }
+//        Book book = bookOpt.get();
+        Book book = this.bookService.get(id).orElseThrow(() -> new RuntimeException("Book was not found"));
+       return this.bookMapper.mapToDto(book);
+    }
+
+    @GetMapping
+    public Page<BookDto> getAll(Pageable pageable){     // pageble - номер запрашиваемой страницы
+        //1. Получаем книги из базы посредством сервиса (entity)
+        //2. Преобразуем entity в dto
+        //3.Список dto возвращаем на фронтент
+//        Page<Book> books = this.bookService.findAll(pageable);
+//        List<BookDto> list = new ArrayList();
+//        for (Book book : books) {
+//            BookDto bookDto = this.bookMapper.mapToDto(book);
+//            list.add(bookDto);
+//        }
+//        Page<BookDto> result = new PageImpl<>(list);
+//        return result;
+
+        return this.bookService.findAll(pageable).map(this.bookMapper::mapToDto);
+    }
+
+    @PutMapping (value = "/{id}")
+    public BookDto updateBook(@PathVariable Long id, @RequestBody BookDto bookDto){
+        // 1. находим книгу в базе по id --> entity
+        // 2. dto --> new entity
+        // 3. new entity. set (id)
+        // 4. save new entity
+        // 5. dto.set(id)
+        //6. return dto
+
+        this.bookService.get(id).orElseThrow(() -> new RuntimeException("Book was not found"));
+        Book newBook = this.bookMapper.mapToEntity(bookDto);
+        newBook.setId(id);
+        this.bookService.save(newBook);
+        bookDto.setId(id);
+        return bookDto;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public String deleteBook (@PathVariable Long id){
+        Book book = this.bookService.get(id).orElseThrow(() -> new RuntimeException("Book was not found"));
+        this.bookService.delete(book);
+
+        return "Deleted";
+
+    }
+
 }
--- src/main/java/com/oksana/library/services/BookService.java
+++ src/main/java/com/oksana/library/services/BookService.java
@@ -3,6 +3,8 @@ package com.oksana.library.services;
 import com.oksana.library.entities.Book;
 import com.oksana.library.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,6 +13,7 @@ import java.util.Optional;
 public class BookService {
 
     private final BookRepository bookRepository;
+
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
@@ -20,15 +23,20 @@ public class BookService {
         return this.save(book);
     }
 
-    public Optional<Book> get(Long id){
+    public Optional<Book> get(Long id) {
         return this.bookRepository.findById(id);
     }
 
     public Book save (Book book){
-        return this.bookRepository.save(book);
+        return this.bookRepository.save(book); // update
     }
 
     public void delete(Book book){
         this.bookRepository.delete(book);
+    }
+
+    public Page<Book> findAll(Pageable pageable){
+        return this.bookRepository.findAll(pageable);
+
     }
 }
